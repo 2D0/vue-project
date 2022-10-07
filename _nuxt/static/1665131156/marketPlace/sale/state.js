b8151168c0c6c5cdec30deae1076fc19b0b6cc29
2D0 <lo7733ve@naver.com>
2022-10-07 21:18:10 +0900

    feat: 간단한 텍스트 수정

--- _nuxt/static/1665131156/marketPlace/sale/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fvue-project\u002F_nuxt\u002Fstatic\u002F1665131156",layout:"default",error:a,serverRendered:true,routePath:"\u002FmarketPlace\u002Fsale",config:{_app:{basePath:"\u002Fvue-project\u002F",assetsPath:"\u002Fvue-project\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- _nuxt/static/1665131156/myPage/userItem/list/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fvue-project\u002F_nuxt\u002Fstatic\u002F1665131156",layout:"default",error:a,serverRendered:true,routePath:"\u002FmyPage\u002FuserItem\u002Flist",config:{_app:{basePath:"\u002Fvue-project\u002F",assetsPath:"\u002Fvue-project\u002F_nuxt\u002F",cdnURL:a}}}}(null));
