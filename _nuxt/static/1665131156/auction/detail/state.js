a34c52aaf64a294aaa9cd53f40570b755bf71517
2D0 <lo7733ve@naver.com>
2022-10-07 18:07:25 +0900

    feat: 경로 변경

---
+++ _nuxt/static/1665131156/auction/detail/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fvue-project\u002F_nuxt\u002Fstatic\u002F1665131156",layout:"default",error:a,serverRendered:true,routePath:"\u002Fauction\u002Fdetail",config:{_app:{basePath:"\u002Fvue-project\u002F",assetsPath:"\u002Fvue-project\u002F_nuxt\u002F",cdnURL:a}}}}(null));
